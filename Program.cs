f5eed474154478c412c9b48c6588b1611bed2d22
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-28 19:30:31 +0530

    [Mukhesh].Add Method to add frequency of element in HashTable

--- Program.cs
+++ Program.cs
@@ -6,16 +6,24 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            MyMapNode<int,string> myMapNode = new MyMapNode<int,string>(5);
-            int key = 1;
+            MyMapNode<string,int> myMapNode = new MyMapNode<string,int>(5);
             string input = "Paranoids are not paranoid because they are paranoid but because " +
                 "they keep putting themselves deliberately into paranoid avoidable situations";
             string[] sample = input.Split(' ');
             foreach (string s in sample)
             {
-                myMapNode.Add(key, s);
-                key++;
+                int count = 0;
+                foreach (string m in sample)
+                {
+                    if (s.Equals(m))
+                        count++;
+                }
+                myMapNode.Add(s,count);
+               
             }
+
+
+
             myMapNode.Display();
             myMapNode.Frequency("paranoid");
             myMapNode.RemoveElement("avoidable");
